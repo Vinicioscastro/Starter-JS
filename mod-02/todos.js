74af931205680445df81b4debf34846243d85027
Vinícios Castro <Viniciospcastro19@gmail.com>
2020-08-24 18:53:02 -0300

    modulo 02 quase completo, pos o desafio 'todo' foi completado com sucesso

--- mod-02/todos.js
+++ mod-02/todos.js
@@ -1,1 +1,51 @@
-alert("Funcionou!!!!")
+var listElement = document.querySelector('#app ul')
+var inputElement = document.querySelector('#app input')
+var buttonElement = document.querySelector('#app button')
+
+var todos = [
+'acordar',
+'fazer cafe',
+'estudar java script'
+]
+
+function renderTodo(){
+    listElement.innerHTML = '';
+    for(todo of todos){
+        var todoElement = document.createElement('li')
+        var todoTexto = document.createTextNode(todo)
+
+        var linkElement= document.createElement('a')     
+
+        linkElement.setAttribute('href', '#')
+
+        var posicao = todos.indexOf(todo)
+        linkElement.setAttribute('onclick', 'deleteTodo('+ posicao +')')
+
+        var linkText = document.createTextNode('excluir')
+
+        linkElement.appendChild(linkText)
+
+        todoElement.appendChild(todoTexto)
+        todoElement.appendChild(linkElement)
+
+        listElement.appendChild(todoElement)
+    }
+
+
+}
+
+renderTodo()
+
+function addTodo(){
+    var todoText = inputElement.value
+    todos.push(todoText)
+    inputElement.value = ''
+    renderTodo()
+}
+
+buttonElement.onclick = addTodo
+
+function deleteTodo(posicao){
+    todos.splice(posicao, 1)
+    renderTodo()
+}
