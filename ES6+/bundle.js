05af4dc2b7b2ee5c0ae6b39e38f9b900e4cdb28e
Vinícios Castro <Viniciospcastro19@gmail.com>
2020-08-26 17:09:52 -0300

    es com desestruturação

--- ES6+/bundle.js
+++ ES6+/bundle.js
@@ -28,30 +28,76 @@ var minhaLista = new todolist()
 document.getElementById('botao').onclick = function () {
     minhaLista.add()
     minhaLista.mostrarusuario()
-} */
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // map ele vai percorrer todo o vetor, posicao por posicao:
+} 
 
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr); // reduce vai transformar em 1 unico valor
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum); // filter vai pegar os pares pos ele retorna um true ou false
+// map ele vai percorrer todo o vetor, posicao por posicao:
+const newArr = arr.map(function (item) {
+    return item * 2
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); // find saber se existe uma informação e encontrar ela no array
+})
+console.log(newArr)
 
-var find = arr.find(function (item) {
-  return item === 6;
-});
-console.log(find); // arrow function em funsao anonima 
+// reduce vai transformar em 1 unico valor
+const sum = arr.reduce(function(total, next){
+    return total + next
 
-var findd = arr.find(function (item) {
-  return item === 2;
-});
-console.log(findd);
+})
+console.log(sum)
+
+// filter vai pegar os pares pos ele retorna um true ou false
+
+const filter = arr.filter(function(item){
+    return item % 2 === 0
+
+})
+console.log(filter)
+
+// find saber se existe uma informação e encontrar ela no array
+
+const find = arr.find(function(item){
+return item === 6
+
+})
+console.log(find)
+// arrow function em funsao anonima 
+const findd = arr.find(item => item === 2)
+console.log(findd)
+
+
+const soma = (x=1, y=6) => x+y
+
+console.log(soma(5, 3))
+console.log(soma(6))
+console.log(soma())
+
+*/
+var usuario = {
+  nome: 'iker',
+  idade: 20,
+  endereco: {
+    rua: 'rua sao judas thadeu',
+    numero: 202,
+    cidade: 'Duque top city'
+  }
+};
+/* const nome = usuario.nome
+const idade = usuario.idade
+const cdd = usuario.endereco.cidade
+console.log(nome, idade, cdd) */
+
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade; // conceito de desinstruturação 
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+
+function mostraNome(_ref) {
+  var idade = _ref.idade;
+  console.log(idade);
+}
+
+mostraNome(usuario);
--- ES6+/main.js
+++ ES6+/main.js
@@ -26,7 +26,7 @@ var minhaLista = new todolist()
 document.getElementById('botao').onclick = function () {
     minhaLista.add()
     minhaLista.mostrarusuario()
-} */
+} 
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -64,7 +64,37 @@ const findd = arr.find(item => item === 2)
 console.log(findd)
 
 
+const soma = (x=1, y=6) => x+y
 
+console.log(soma(5, 3))
+console.log(soma(6))
+console.log(soma())
 
+*/
 
+const usuario = {
+    nome: 'iker',
+    idade: 20,
+    endereco: {
+        rua: 'rua sao judas thadeu',
+        numero: 202,
+        cidade: 'Duque top city'
+    }
+}
 
+/* const nome = usuario.nome
+const idade = usuario.idade
+const cdd = usuario.endereco.cidade
+console.log(nome, idade, cdd) */
+
+const { nome, idade, endereco: {cidade} } = usuario // conceito de desinstruturação 
+
+console.log(nome)
+console.log(idade)
+console.log(cidade)
+
+function mostraNome ({idade}){
+    console.log(idade)
+}
+
+mostraNome(usuario)
