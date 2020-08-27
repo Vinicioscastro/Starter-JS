89d800ea163cfc567247e52f6f01b11a2397936b
Vinícios Castro <Viniciospcastro19@gmail.com>
2020-08-27 10:59:54 -0300

    fim da part 01

--- ES6+/part 01/bundle.js
+++ ES6+/part 01/bundle.js
@@ -1,3 +1,11 @@
+"use strict";
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 /* class list {
     constructor() {
         this.todo = [];
@@ -131,23 +139,25 @@ const arr2= [4, 5, 6]
 const arr3 = [...arr, ...arr2]
 
 console.log(arr3)*/
-const usuario1 = {
+var usuario1 = {
   nome: "vinicios",
   idade: 21,
   empresa: 'ifma'
 };
-const usuario2 = { ...usuario1,
+
+var usuario2 = _objectSpread(_objectSpread({}, usuario1), {}, {
   nome: 'iker'
-};
+});
+
 console.log(usuario1);
 console.log(usuario2);
-console.log(`meu nome e ${usuario1.nome}, trabalho no ${usuario1.empresa} e tenho ${usuario1.idade} anos`); // Sintaxe curta de objeto
+console.log("meu nome e ".concat(usuario1.nome, ", trabalho no ").concat(usuario1.empresa, " e tenho ").concat(usuario1.idade, " anos")); // Sintaxe curta de objeto
 
-const nome = 'iker';
-const idade = 21;
-const estudante = {
-  nome,
-  idade,
+var nome = 'iker';
+var idade = 21;
+var estudante = {
+  nome: nome,
+  idade: idade,
   empresa: "prefeitura"
 };
 console.log(estudante);
